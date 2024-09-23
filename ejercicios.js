d3c7f69f5d5a1f68816391090d982bc66452faec
KevinSebaLee <48044115@est.ort.edu.ar>
2024-09-23 10:24:58 -0300

    Comparando datos y tipos

--- ejercicios.js
+++ ejercicios.js
@@ -9,3 +9,35 @@ function reemplazarPalabras(){
     document.getElementById('ejercicio1').innerHTML = `Primera oracion: ${reemplazarPalabras}, Palabra a cambiar: ${palabraClave}, Palabra elegida: ${palabraIngresar}, Oracion Final: ${nuevaOracion}`;
 }
 
+function compararDatos(){
+    let numeroCompararAString;
+    let numeroMismoTipoValor;
+    let tipoValor;
+    let probarBool;
+
+    if(10 == '10'){
+        numeroCompararAString = true;
+    }
+    else{
+        numeroCompararAString = false;
+    }
+    
+    if(10 === '10'){
+        numeroMismoTipoValor = true;
+    }
+    else{
+        numeroMismoTipoValor = false;
+    }
+
+    tipoValor = typeof(10.6);
+
+    if(true == 1){
+        probarBool = true;
+    }
+    else{
+        probarBool = false;
+    }
+
+    document.getElementById('ejercicio2').innerHTML = `A: ${numeroCompararAString}; B: ${numeroMismoTipoValor}; C: ${tipoValor}; D: ${probarBool}`;
+}
+
